976f573b3dac1cbc5047dd3455a14e9e706cbf33
Ishant Pal <mrishantsre1511ish@gmail.com>
2022-12-15 18:48:26 +0530

    Assignment-2;

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
+
+ class A{
+    public String meth()
+    {
+        return "Invoking method from class A";
+    }
+}
+ class B extends A{
+     public String meth()
+     {
+         return "Invoking method from class B";
+     }
+}
 public class Main {
-  
+    public static void main(String args[])
+   {
+     B b = new B();
+     System.out.print(b.meth());
+     B b1=new B();
+     System.out.print(b.meth());
+   }
 }
