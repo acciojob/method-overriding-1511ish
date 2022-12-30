256007fbf9d1ba491b657b002c1e3156afe16f47
Ishant Pal <mrishantsre1511ish@gmail.com>
2022-12-30 12:39:57 +0530

    Messages

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,21 +3,21 @@ package com.driver;
 
 
 public class Main {
-    public static class A{
-        public String meth()
+        public static class A{
+              public String meth()
         {
             return "Invoking method from class A";
         }
-        }public static class B extends A{
-        public String meth()
+        }
+        public static class B extends A{
+              public String meth()
         {
             return "Invoking method from class B";
         }
-    }    public static void main(String args[])
+        }
+    public static void main(String args[])
    {
      B b = new B();
-     System.out.print(b.meth());
-     B b1=new B();
      System.out.print(b.meth());
    }
 }
